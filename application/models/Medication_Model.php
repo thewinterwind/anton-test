32bd0e48ec4c1668a77305bba42b9a18796dc3a4
prympineda <prympineda@gmail.com>
2020-06-09 14:57:45 +0800

    update

--- application/models/Medication_Model.php
+++ application/models/Medication_Model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Medication_Model extends CI_Model {
+class Medication_model extends CI_Model {
 
     function __construct()
     {
